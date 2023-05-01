13273b6ff948c56975fe9ba1ea47a6937076bc0b
tung-sh <tungntfx08789@funix.edu.vn>
2023-05-01 21:37:30 +0700

    create pbl respitory

---
+++ extension.c
@@ -0,0 +1,5 @@
+#include "book.c"
+
+Book find_by_author(char*); 
+Book find_by_isbn_name(char*); 
+void filter_publisher(); 
---
+++ manage_status.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void update(); 
+void edit(); 
+void sort(); 
